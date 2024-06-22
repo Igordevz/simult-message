79668e3316c318ec30d77e47e30c08c9df826a64
Igordevz <igortza.98483@gmail.com>
2024-06-22 11:41:01 -0300

    install dependencies

--- server/src/controllers/use-cases/create-user.ts
+++ server/src/controllers/use-cases/create-user.ts
@@ -9,27 +9,14 @@ export default async function CreateUser(request: Request, response: Response) {
       email: z.string().email(),
       password: z.string().min(8),
     });
-<<<<<<< HEAD
-    // pedi as informações para o usuario
-    const { name, email, password } = userSchema.parse(request.body);
-    // criei uma configuração de criptografia
-    const salt = await bcrypt.genSalt(12);
-    const passwordHas = await bcrypt.hash(password, salt);
-   // gere um código com  5 dígitos aleatorios
-=======
     const { name, email, password } = userSchema.parse(request.body);
     const salt = await bcrypt.genSalt(12);
     const passwordHas = await bcrypt.hash(password, salt);
->>>>>>> master
     function generateRandomCode() {
       let randomCode = Math.floor(10000 + Math.random() * 90000);
       return randomCode;
     }
     let randomCode = generateRandomCode();
-<<<<<<< HEAD
-    // db
-=======
->>>>>>> master
     const userExist = await prisma.user.findUnique({
       where: {
         email: email,
@@ -42,26 +29,16 @@ export default async function CreateUser(request: Request, response: Response) {
       });
     }
 
-<<<<<<< HEAD
-    await prisma.user.create({
-      data: {
-      name,
-=======
     const data = await prisma.user.create({
       data: {
         name,
->>>>>>> master
         email,
         password: passwordHas,
         mailer: randomCode.toString(),
       },
     });
 
-<<<<<<< HEAD
-    return response.status(201).json({ msg: "usuário criado com sucesso" });
-=======
     return response.status(201).json({ msg: "usuário criado com sucesso", details: data });
->>>>>>> master
   } catch (error) {
     console.log(error);
     return response
